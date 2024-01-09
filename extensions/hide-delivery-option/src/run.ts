5413b368779d56f3aea2f0ab1d9e61250b6762da
Rafael Granados <rafandresgrana2@gmail.com>
2024-01-09 15:17:07 -0500

    removed comments

--- extensions/hide-delivery-option/src/run.ts
+++ extensions/hide-delivery-option/src/run.ts
@@ -4,14 +4,11 @@ import type {
 } from "../generated/api";
 
 export function run(input: RunInput): FunctionRunResult {
-  // Check if the customer has the 'hide' tag
   const hideDeliveryOption = input.cart?.buyerIdentity?.customer?.hasAnyTag;
 
   if (hideDeliveryOption) {
-    // Collect the delivery options
     let toHide = input.cart.deliveryGroups
       .flatMap(group => group.deliveryOptions)
-      // Construct a hide operation for each delivery option
       .filter(option => option.title === 'Standard')
       .map(option => ({
         hide: {
@@ -19,13 +16,11 @@ export function run(input: RunInput): FunctionRunResult {
         }
       }));
 
-    // Return the operations
     return {
       operations: toHide
     };
   }
 
-  // If the customer doesn't have the 'hide' tag, no operations are needed
   return {
     operations: []
   };
