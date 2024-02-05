24a9cd984349c8b336869b5aa69d6643bda601d7
Rafael Granados <rafandresgrana2@gmail.com>
2024-02-05 16:35:36 -0500

    update app

--- extensions/hide-delivery-option/src/run.ts
+++ extensions/hide-delivery-option/src/run.ts
@@ -4,33 +4,39 @@ import type {
 } from "../generated/api";
 
 export function run(input: RunInput): FunctionRunResult {
-  const hideStandard = input.cart?.attribute?.value === 'hideStandard';
-
-  if (hideStandard) {
-    let toHide = input.cart.deliveryGroups
+  if (input.cart.deliveryGroups.some(group => group.deliveryAddress?.countryCode === 'AT')) {
+    const hideStandard = input.cart?.attribute?.value === 'hideStandard';
+  
+    if (hideStandard) {
+      let toHide = input.cart.deliveryGroups
+        .flatMap(group => group.deliveryOptions)
+        .filter(option => option.title === 'Standardversand Österreich')
+        .map(option => ({
+          hide: {
+            deliveryOptionHandle: option.handle
+          }
+        }));
+  
+      return {
+        operations: toHide
+      };
+    } else {
+      let toHide = input.cart.deliveryGroups
       .flatMap(group => group.deliveryOptions)
-      .filter(option => option.title === 'Standard')
+      .filter(option => option.title === 'Standard Versand Österreich')
       .map(option => ({
         hide: {
           deliveryOptionHandle: option.handle
         }
       }));
-
-    return {
-      operations: toHide
-    };
+  
+      return {
+        operations: toHide
+      };
+    }
   } else {
-    let toHide = input.cart.deliveryGroups
-    .flatMap(group => group.deliveryOptions)
-    .filter(option => option.title === 'Express')
-    .map(option => ({
-      hide: {
-        deliveryOptionHandle: option.handle
-      }
-    }));
-
     return {
-      operations: toHide
-    };
+      operations: []
+    }
   }
 }
